1b04c552d5e0c05e58b71bca6af53257ea79023d
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2022-05-07 22:31:00 +0200

    Bootstrap expo app

---
+++ apps/authenticator/jest.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'authenticator',
+  preset: '../../jest.preset.ts',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/authenticator',
+};
