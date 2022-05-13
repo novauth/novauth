aa49b4453f7cf488880a6219f4d16c0014c2f72a
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2022-05-13 23:40:32 +0200

    bootstrap react-native-elements, add homepage fab

--- apps/authenticator/src/screens/Home.tsx
+++ apps/authenticator/src/screens/Home.tsx
@@ -1,16 +1,30 @@
 import { StackScreenProps } from '@react-navigation/stack'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, Text, View } from 'react-native'
 import { RootStackNavigatorParamList } from '../navigation/RootStackNavigator'
+import { FAB } from '@rneui/base'
 
 type HomeProps = StackScreenProps<RootStackNavigatorParamList, 'Home'>
 
 function Home({}: HomeProps) {
   return (
-    <SafeAreaView>
+    <View style={styles.container}>
       <Text>NovAuth: Securing the Modern Web</Text>
-    </SafeAreaView>
+      <FAB
+        visible={true}
+        onPress={() => {}}
+        icon={{ name: 'add', color: 'white' }}
+        color='blue'
+        placement='right'
+      />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 5,
+    flexGrow: 1,
+  },
+})
 
 export default Home
