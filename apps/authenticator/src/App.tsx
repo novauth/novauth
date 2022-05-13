b3321e1fc81332be7c20f90fd5a3a964f412f0d5
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2022-05-13 22:30:27 +0200

    remove boilerplate, init project structure

---
+++ apps/authenticator/src/App.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>NovAuth: Securing the Modern Web</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
